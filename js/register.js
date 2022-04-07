74b74f8c7083ae1c36643f679d33fa735455bf97
Meenssi <fumikoe439@gmail.com>
2022-04-08 01:45:05 +0700

    update index.html
    เพิ่มส่วนของ product สินค้า

--- js/register.js
+++ js/register.js
@@ -1,7 +1,6 @@
 function Fun_1() {
-    var x = document.createElement("INPUT");
-    var i = 2;
-    i = i+1;
+    var x = document.createElement("input");
+    
     x.setAttribute("type", "text");
     document.body.appendChild(x);
 }
