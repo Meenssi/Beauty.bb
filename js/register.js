f6073ba410b743074b6ece0030f8130fce4b8a18
Meenssi <fumikoe439@gmail.com>
2022-04-12 00:52:50 +0700

    ทำหน้า profile ต่อ
    - ทำหน้า profile ต่อ
    - แก้ไขหน้า register (css) เล็กน้อย
    -
    -

--- js/register.js
+++ js/register.js
@@ -4,3 +4,11 @@ function Fun_1() {
     x.setAttribute("type", "text");
     document.body.appendChild(x);
 }
+
+function objForIn() {
+    let nameValue = document.getElementById("name").Value;
+    document.getElementById("name_").innerHTML = "name : " + nameValue;
+    let usernameValue = document.getElementById("username").Value;
+    let emailValue = document.getElementById("email").value;
+    
+}
